5f33f1551854bb8b68d7872ac4dc9f4dba6978b5
Alexandr Ostrovny <sanny1603@gmail.com>
2023-11-08 17:04:49 +0200

    Passed Streaming chapter

--- app/ui/dashboard/cards.tsx
+++ app/ui/dashboard/cards.tsx
@@ -3,8 +3,9 @@ import {
   ClockIcon,
   UserGroupIcon,
   InboxIcon,
-} from '@heroicons/react/24/outline';
-import { lusitana } from '@/app/ui/fonts';
+} from "@heroicons/react/24/outline";
+import { lusitana } from "@/app/ui/fonts";
+import { fetchCardData } from "@/app/lib/data";
 
 const iconMap = {
   collected: BanknotesIcon,
@@ -14,18 +15,24 @@ const iconMap = {
 };
 
 export default async function Cards() {
+  const {
+    numberOfCustomers,
+    numberOfInvoices,
+    totalPaidInvoices,
+    totalPendingInvoices,
+  } = await fetchCardData();
   return (
     <>
       {/* NOTE: comment in this code when you get to this point in the course */}
 
-      {/* <Card title="Collected" value={totalPaidInvoices} type="collected" />
-      <Card title="Pending" value={totalPendingInvoices} type="pending" />
-      <Card title="Total Invoices" value={numberOfInvoices} type="invoices" />
+      <Card title='Collected' value={totalPaidInvoices} type='collected' />
+      <Card title='Pending' value={totalPendingInvoices} type='pending' />
+      <Card title='Total Invoices' value={numberOfInvoices} type='invoices' />
       <Card
-        title="Total Customers"
+        title='Total Customers'
         value={numberOfCustomers}
-        type="customers"
-      /> */}
+        type='customers'
+      />
     </>
   );
 }
@@ -37,20 +44,19 @@ export function Card({
 }: {
   title: string;
   value: number | string;
-  type: 'invoices' | 'customers' | 'pending' | 'collected';
+  type: "invoices" | "customers" | "pending" | "collected";
 }) {
   const Icon = iconMap[type];
 
   return (
-    <div className="rounded-xl bg-gray-50 p-2 shadow-sm">
-      <div className="flex p-4">
-        {Icon ? <Icon className="h-5 w-5 text-gray-700" /> : null}
-        <h3 className="ml-2 text-sm font-medium">{title}</h3>
+    <div className='rounded-xl bg-gray-50 p-2 shadow-sm'>
+      <div className='flex p-4'>
+        {Icon ? <Icon className='h-5 w-5 text-gray-700' /> : null}
+        <h3 className='ml-2 text-sm font-medium'>{title}</h3>
       </div>
       <p
         className={`${lusitana.className}
-          truncate rounded-xl bg-white px-4 py-8 text-center text-2xl`}
-      >
+          truncate rounded-xl bg-white px-4 py-8 text-center text-2xl`}>
         {value}
       </p>
     </div>
